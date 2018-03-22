48751e6f77d65325cd07a5a58294135fd484982f
Elijah Rosier <elijah@age-of-aincrad.com>
2018-03-22 12:15:18 +0000

    Removing eclipse error on invalidloginexception

--- src/me/elijahrosier/cos/datagrabber/exceptions/InvalidLoginException.java
+++ src/me/elijahrosier/cos/datagrabber/exceptions/InvalidLoginException.java
@@ -2,4 +2,6 @@ package me.elijahrosier.cos.datagrabber.exceptions;
 
 public class InvalidLoginException extends Exception {
 
+	private static final long serialVersionUID = -6356188135192870337L;
+
 }
