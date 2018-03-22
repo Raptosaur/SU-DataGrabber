997351658d7394ba9e7aff2b00260afa4aa191aa
Elijah Rosier <elijah@age-of-aincrad.com>
2018-03-22 12:18:50 +0000

    Added getUsersTimetable with specified date.

--- src/me/elijahrosier/cos/datagrabber/timetable/Timetable.java
+++ src/me/elijahrosier/cos/datagrabber/timetable/Timetable.java
@@ -9,12 +9,52 @@ import org.jsoup.select.Elements;
 import me.elijahrosier.cos.datagrabber.User;
 
 public class Timetable {
-	
+
 	public static TimetableSlot[][] getUsersTimetable(User user){
 		TimetableSlot[][] modules = new TimetableSlot[9][5];
 		
 		try {
 			Document doc = user.getPage("intranet/attendance/timetable");
+			
+			Element table = doc.selectFirst("#timetable");
+			
+			if(table != null){
+				Elements rows = table.select("tr");
+				
+				for(int i = 0; i < rows.size(); i++){
+					if(i != 0){
+						Element row = rows.get(i);
+						
+						Elements days = row.select("td");
+						
+						for(int j = 0; j < days.size(); j++){
+							
+							Element slot = days.get(j).selectFirst(".lecture");
+							if(slot != null){
+								String moduleName = slot.selectFirst("strong").text();
+								String moduleLecturer = slot.selectFirst("span") != null ? slot.selectFirst("span").text() : "Unknown";
+								String moduleLocation = slot.selectFirst(".lectureinfo.room") != null ? slot.selectFirst(".lectureinfo.room").text() : "Unknown";
+								
+								modules[i-1][j] = new TimetableSlot(moduleName, moduleLecturer, moduleLocation);
+							}
+							
+						}
+					}
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return modules;
+	}
+	
+	
+	public static TimetableSlot[][] getUsersTimetable(User user, int year, int month, int day){
+		TimetableSlot[][] modules = new TimetableSlot[9][5];
+		
+		try {
+			Document doc = user.getPage("intranet/attendance/timetable/"+year+"/"+(month < 10 ? "0"+month : month)+"/"+(day < 10 ? "0"+day : day));
 			
 			Element table = doc.selectFirst("#timetable");
 			
