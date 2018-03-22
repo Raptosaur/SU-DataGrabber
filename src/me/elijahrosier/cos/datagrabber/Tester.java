a1a794f31b28a89b59e01788a1381d5ddc76853c
Elijah Rosier <elijah@age-of-aincrad.com>
2018-03-22 12:12:04 +0000

    Including tester file

---
+++ src/me/elijahrosier/cos/datagrabber/Tester.java
@@ -0,0 +1,31 @@
+package me.elijahrosier.cos.datagrabber;
+
+import me.elijahrosier.cos.datagrabber.exceptions.InvalidLoginException;
+import me.elijahrosier.cos.datagrabber.timetable.Timetable;
+import me.elijahrosier.cos.datagrabber.timetable.TimetableSlot;
+
+public class Tester {
+
+	public static void main(String[] args){
+		User u = null;
+		try {
+			u = new User("123456", "45678910");
+		} catch (InvalidLoginException e) {
+			e.printStackTrace();
+		}
+		if(u != null){
+			System.out.println("Logged in as "+u.getUsername()+" ("+u.getName()+")");
+			
+			TimetableSlot[][] timetable = Timetable.getUsersTimetable(u);
+			
+			for(int i = 0; i< 5; i++){
+				System.out.println("--------------------------------------");
+				for(int j = 0; j < 9; j++){
+					System.out.println((j+9)+":00 " + (timetable[j][i] != null ? timetable[j][i] : ""));
+				}
+			}
+			
+		}
+	}
+	
+}
